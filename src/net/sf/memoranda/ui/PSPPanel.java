a0ba6072f72a02c109d9c0795668bcd695e4c3fd
Brandon Jackson <bgjackso@asu.edu>
2017-03-13 13:39:36 -0700

    Creation of PSP
    PSP panel created

---
+++ src/net/sf/memoranda/ui/PSPPanel.java
@@ -0,0 +1,69 @@
+package net.sf.memoranda.ui;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.JOptionPane;
+
+import net.sf.memoranda.CurrentProject;
+import net.sf.memoranda.EventNotificationListener;
+import net.sf.memoranda.EventsManager;
+import net.sf.memoranda.EventsScheduler;
+import net.sf.memoranda.History;
+import net.sf.memoranda.NoteList;
+import net.sf.memoranda.Project;
+import net.sf.memoranda.ProjectListener;
+import net.sf.memoranda.ProjectManager;
+import net.sf.memoranda.ResourcesList;
+import net.sf.memoranda.TaskList;
+import net.sf.memoranda.date.CalendarDate;
+import net.sf.memoranda.date.CurrentDate;
+import net.sf.memoranda.date.DateListener;
+import net.sf.memoranda.util.AgendaGenerator;
+import net.sf.memoranda.util.CurrentStorage;
+import net.sf.memoranda.util.Local;
+import net.sf.memoranda.util.Util;
+import nu.xom.Element;
+
+public class PSPPanel extends JPanel {
+	BorderLayout borderLayout = new BorderLayout();
+	JToolBar toolBar = new JToolBar();
+	JScrollPane scrollPane = new JScrollPane();
+	DailyItemsPanel parentPanel = null;
+	
+	
+	public PSPPanel(DailyItemsPanel _parentPanel){
+		try {
+			parentPanel = _parentPanel;
+		} catch (Exception ex) {
+			new ExceptionDialog(ex);
+		}
+	}
+	
+	void jbInit() throws Exception {
+		toolBar.setFloatable(false);
+		this.setLayout(borderLayout);
+		this.add(scrollPane, BorderLayout.CENTER);
+		this.add(toolBar, BorderLayout.NORTH);
+		scrollPane.getViewport().setBackground(Color.white);
+	}
+		
+		
+}
+
+
+
