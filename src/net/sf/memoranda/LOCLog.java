16aee9fcc97865aa8e3861a55212fb840071f6ff
Brandon Jackson <bgjackso@asu.edu>
2017-03-29 20:24:06 -0700

    thoughtd

---
+++ src/net/sf/memoranda/LOCLog.java
@@ -0,0 +1,14 @@
+package net.sf.memoranda;
+
+public class LOCLog {
+
+	/*
+	 * Get value from text box.
+	 * 
+	 * get current day
+	 * 
+	 * get time
+	 * 
+	 * save to a project and all other places in a log list (another class?)
+	 */
+}
