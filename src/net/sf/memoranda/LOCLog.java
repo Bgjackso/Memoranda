b5d0479cbfbf088fcf230eee3687a1ba36315d04
Brandon Jackson <bgjackso@asu.edu>
2017-03-31 01:16:25 -0700

    a

--- src/net/sf/memoranda/LOCLog.java
+++ src/net/sf/memoranda/LOCLog.java
@@ -1,14 +1,20 @@
 package net.sf.memoranda;
 
+import java.util.Vector;
+
+import nu.xom.Document;
+
 public class LOCLog {
 
-	/*
-	 * Get value from text box.
-	 * 
-	 * get current day
-	 * 
-	 * get time
-	 * 
-	 * send to project?
-	 */
+	Vector<LinesofCode> getAllLOC();
+    
+    LinesofCode getLOC(String p_defect_num);
+    
+    void addLOC(LinesofCode locAmount);
+    
+    void removeLOC(String locAmount);
+        
+    int getTotalLOCCount();
+    
+    Document getXMLContent();
 }
