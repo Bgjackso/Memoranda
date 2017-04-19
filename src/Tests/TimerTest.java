a6a9269b7091a36f4e9bcb4077d083e9df17af4e
tnklassen <tklassen@asu.edu>
2017-04-19 13:07:29 -0700

    TimerTest components added
    Some components of TimerTest have been added to TimerTest file.  Final
    version of test to come in next commit.

--- src/Tests/TimerTest.java
+++ src/Tests/TimerTest.java
@@ -2,33 +2,70 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import javax.swing.JLabel;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TimerTest {
+import net.sf.memoranda.PSPTimer;
+//import net.sf.memoranda.ui.PSPPanel.PSPTimer;
 
+public class TimerTest{
+	private Thread runThread;
+	private boolean running;
+	private boolean paused;
+    private long summedTime = 0;
+	  
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		JLabel time = new JLabel("Time");
+		PSPTimer timer = new PSPTimer();
+		//have not imported PSPTimer from PSPPanel class
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-	}
+	} //dont touch
 
 	@Before
 	public void setUp() throws Exception {
+		running = true;
+	    paused = false;
+	    runThread = new Thread();
+	    runThread.start();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	}
+	} //dont touch
 
+	/*public void run() {
+		/*long startTime = System.currentTimeMillis();
+		      
+		while(running && !paused) {
+			//update(summedTime + (System.currentTimeMillis() - startTime)); //timer causing issues
+		}
+		/*if(paused){
+			summedTime += System.currentTimeMillis() - startTime;
+		}else{
+			summedTime = 0;
+		} 
+	}
+	
+	/*@Test
+	public void update(long dTime){
+		  time.setText(String.valueOf(String.valueOf((dTime/1000)/60) + ":" 
+				  + String.valueOf((dTime/1000)%60) + ":" + String.valueOf((dTime)%1000)));
+	  } */
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test(){
+		//assert summedTime not null?
+		//assert update value not null?
+		assertNotNull(summedTime);
+		//assertNotSame(0, summedTime); ??
 	}
-
 }
